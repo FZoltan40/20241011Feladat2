049ba2e61f4a7063d504f65df30bfefa378d1276
FZoltan40 <farkasz@kkszki.hu>
2024-10-11 13:16:24 +0200

    Koszon method is ready.

--- Program.cs
+++ Program.cs
@@ -8,8 +8,32 @@ namespace Feladat2
 {
     internal class Program
     {
+        public static void koszon()
+        {
+           
+            int hour = DateTime.Now.Hour;
+            Console.WriteLine(hour);
+
+            if (hour < 5)
+            {
+                Console.WriteLine("Jó éjszakát.");
+            }
+            else if (hour < 12)
+            {
+                Console.WriteLine("Jó reggelt.");
+            }
+            else if (hour < 18)
+            {
+                Console.WriteLine("Jó napot.");
+            }
+            else if (hour < 24)
+            {
+                Console.WriteLine("Jó estét.");
+            }
+        }
         static void Main(string[] args)
         {
+            koszon();
         }
     }
 }
